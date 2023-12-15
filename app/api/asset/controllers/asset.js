5d0d58e56175bd16009924f144758f66808b7c44
pablohegoburu <pablo@paretofrontier.ai>
2023-12-15 17:38:40 -0300

    24 h price difference

--- app/api/asset/controllers/asset.js
+++ app/api/asset/controllers/asset.js
@@ -4,6 +4,8 @@
  * asset controller
  */
 
+const moment = require("moment");
+
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::asset.asset", ({ strapi }) => ({
@@ -16,8 +18,27 @@ module.exports = createCoreController("api::asset.asset", ({ strapi }) => ({
       orderBy: { createdAt: "desc" },
     });
 
+    // Obtener la fecha actual y la fecha de hace 24 horas
+    const currentDate = moment();
+    const pastDate = moment().subtract(24, "hours");
+
+    const { price: price_24h } = await strapi.db
+      .query("api::asset.asset")
+      .findOne({
+        select: ["price"],
+        where: {
+          name,
+          createdAt: {
+            $gte: pastDate.toISOString(), // Filtrar por fechas mayores o iguales a hace 24 horas
+            $lte: currentDate.toISOString(), // Filtrar por fechas menores o iguales a la fecha actual
+          },
+        },
+        orderBy: { createdAt: "desc" },
+      });
+
     return ctx.send({
       price,
+      last_24h: price_24h !== undefined ? price_24h - price : null,
     });
   },
 }));
