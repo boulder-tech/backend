6417d035f2fec58aebde4e2fb91a4e42ef51880a
pablohegoburu <pablo@paretofrontier.ai>
2024-01-08 17:37:55 -0300

    adding id and full transaction sent

--- app/api/transaction/controllers/transaction.js
+++ app/api/transaction/controllers/transaction.js
@@ -1,9 +1,52 @@
-'use strict';
+"use strict";
 
 /**
  * transaction controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::transaction.transaction');
+module.exports = createCoreController(
+  "api::transaction.transaction",
+  ({ strapi }) => ({
+    async fullTransaction(ctx) {
+      const { amount_stable, token, address, hash, type_stable } =
+        ctx.request.body;
+
+      const client = await this.getByPublicAddress(address);
+
+      const status = "pending_mint";
+
+      await strapi.db.query("api::transaction.transaction").create({
+        data: {
+          client_id: client.id,
+          amount_stable,
+          token,
+          address,
+          hash,
+          status,
+          type_stable,
+        },
+      });
+
+      return ctx.send({
+        success: true,
+      });
+    },
+    async getByPublicAddress(address) {
+      const existingAddress = await strapi.db
+        .query("api::public-address.public-address")
+        .findOne({
+          where: { address },
+          populate: { client: true },
+        });
+
+      if (existingAddress) {
+        const { client } = existingAddress;
+        return client;
+      } else {
+        return null;
+      }
+    },
+  })
+);
--- app/api/transaction/routes/transaction.js
+++ app/api/transaction/routes/transaction.js
@@ -1,13 +1,13 @@
 module.exports = {
-    routes: [
-        {
-            method: 'POST',
-            path: '/transaction',
-            handler: 'api::transaction.transaction.create',
-            config: {
-                auth: false,
-                policies: [],
-            },
-        },
-    ],
+  routes: [
+    {
+      method: "POST",
+      path: "/transaction",
+      handler: "api::transaction.transaction.fullTransaction", //create',
+      config: {
+        auth: false,
+        policies: [],
+      },
+    },
+  ],
 };
