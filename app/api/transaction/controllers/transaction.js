44964b6e9d69c5dddcd6b0a7c0eff5cdfadf1728
Ubuntu <ubuntu@ip-172-31-12-200.us-west-1.compute.internal>
2024-01-15 14:34:18 +0000

    fix transactions by public address endpoint

--- app/api/transaction/controllers/transaction.js
+++ app/api/transaction/controllers/transaction.js
@@ -1,95 +1,100 @@
-"use strict";
+'use strict';
 
 /**
  * transaction controller
  */
 
-const { createCoreController } = require("@strapi/strapi").factories;
+const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController(
-  "api::transaction.transaction",
-  ({ strapi }) => ({
-    async startTransaction(ctx) {
-      const { amount_stable, token, address, hash, type_stable } =
-        ctx.request.body;
+    'api::transaction.transaction',
+    ({ strapi }) => ({
+        async startTransaction(ctx) {
+            const { amount_stable, token, address, hash, type_stable } =
+                ctx.request.body;
 
-      const client = await this.getByPublicAddress(address);
+            const client = await this.getByPublicAddress(address);
 
-      const status = "pending_mint";
+            const status = 'pending_mint';
 
-      await strapi.db.query("api::transaction.transaction").create({
-        data: {
-          client_id: client.id,
-          amount_stable,
-          token,
-          address,
-          hash,
-          status,
-          type_stable,
+            await strapi.db.query('api::transaction.transaction').create({
+                data: {
+                    client_id: client.id,
+                    amount_stable,
+                    token,
+                    address,
+                    hash,
+                    status,
+                    type_stable,
+                },
+            });
+
+            return ctx.send({
+                success: true,
+            });
         },
-      });
+        async getByPublicAddress(address) {
+            const existingAddress = await strapi.db
+                .query('api::public-address.public-address')
+                .findOne({
+                    where: { address },
+                    populate: { client: true },
+                });
 
-      return ctx.send({
-        success: true,
-      });
-    },
-    async getByPublicAddress(address) {
-      const existingAddress = await strapi.db
-        .query("api::public-address.public-address")
-        .findOne({
-          where: { address },
-          populate: { client: true },
-        });
+            if (existingAddress) {
+                const { client } = existingAddress;
+                return client;
+            } else {
+                return null;
+            }
+        },
+        async endTransaction(ctx) {
+            const { token_minted, hash_mint, hash, change } = ctx.request.body;
 
-      if (existingAddress) {
-        const { client } = existingAddress;
-        return client;
-      } else {
-        return null;
-      }
-    },
-    async endTransaction(ctx) {
-      const { token_minted, hash_mint, hash, change } = ctx.request.body;
+            await strapi.db.query('api::transaction.transaction').update({
+                where: { hash: hash },
+                data: {
+                    status: 'tokens_minted',
+                    token_minted,
+                    hash_mint,
+                    change,
+                },
+            });
 
-      await strapi.db.query("api::transaction.transaction").update({
-        where: { hash: hash },
-        data: { status: "tokens_minted", token_minted, hash_mint, change },
-      });
+            return ctx.send({
+                success: true,
+            });
+        },
+        async allTransactions(ctx) {
+            try {
+                const { address } = ctx.params;
 
-      return ctx.send({
-        success: true,
-      });
-    },
-    async allTransactions(ctx) {
-      try {
-        const { address } = ctx.params;
+                const client = await this.getByPublicAddress(address);
 
-        const client = await this.getByPublicAddress(address);
+                if (!client) {
+                    return ctx.send({
+                        success: false,
+                        message: 'Client not found on db',
+                    });
+                }
 
-        if (!client) {
-          return ctx.send({
-            success: false,
-            message: "Client not found on db",
-          });
-        }
+                const transactions = await strapi.db
+                    .query('api::transaction.transaction')
+                    .findMany({
+                        where: { client_id: client.id },
+                    });
 
-        const transactions = await strapi.db
-          .query("api::transaction.transaction")
-          .findMany({
-            client: client.id,
-          });
-
-        return ctx.send({
-          success: true,
-          transactions,
-        });
-      } catch (error) {
-        console.error(error);
-        return ctx.send({
-          success: false,
-          message: "Error occurred when getting all transactions.",
-        });
-      }
-    },
-  })
+                return ctx.send({
+                    success: true,
+                    transactions,
+                });
+            } catch (error) {
+                console.error(error);
+                return ctx.send({
+                    success: false,
+                    message: 'Error occurred when getting all transactions.',
+                });
+            }
+        },
+    })
 );
