0beac8455dd89dc26e52c64bba69a5c1bdd79b5e
Ubuntu <ubuntu@ip-172-31-12-200.us-west-1.compute.internal>
2024-01-16 19:34:02 +0000

    change withPersonaStatus method

--- app/api/client/routes/client.js
+++ app/api/client/routes/client.js
@@ -1,6 +1,6 @@
 module.exports = {
-  routes: [
-    /*
+    routes: [
+        /*
         {
             method: 'POST',
             path: '/client/signup',
@@ -11,86 +11,86 @@ module.exports = {
             },
         },
         */
-    {
-      method: "POST",
-      path: "/client/signup",
-      handler: "api::client.client.signup",
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
-    {
-      method: "PUT",
-      path: "/client/verify-token/:token",
-      handler: "api::client.client.verifyToken",
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
-    {
-      method: "POST",
-      path: "/client/login",
-      handler: "api::client.client.login",
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
-    {
-      method: "POST",
-      path: "/client/connect-wallet",
-      handler: "api::client.client.connectWallet",
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
-    {
-      method: "PUT",
-      path: "/client/kyc",
-      handler: "api::client.client.KYC",
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
-    {
-      method: "PUT",
-      path: "/client/updateData",
-      handler: "api::client.client.updateData",
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
-    {
-      method: "GET",
-      path: "/client/public-address/:address",
-      handler: "api::client.client.getByPublicAddress",
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
-    {
-      method: "POST",
-      path: "/client/kyc/one-time-link",
-      handler: "api::client.client.generateOneTimeLinkForKyc",
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
-    {
-      method: "GET",
-      path: "/client/kyc/withPersonaStatus",
-      handler: "api::client.client.withPersonaStatus",
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
-  ],
+        {
+            method: 'POST',
+            path: '/client/signup',
+            handler: 'api::client.client.signup',
+            config: {
+                auth: false,
+                policies: [],
+            },
+        },
+        {
+            method: 'PUT',
+            path: '/client/verify-token/:token',
+            handler: 'api::client.client.verifyToken',
+            config: {
+                auth: false,
+                policies: [],
+            },
+        },
+        {
+            method: 'POST',
+            path: '/client/login',
+            handler: 'api::client.client.login',
+            config: {
+                auth: false,
+                policies: [],
+            },
+        },
+        {
+            method: 'POST',
+            path: '/client/connect-wallet',
+            handler: 'api::client.client.connectWallet',
+            config: {
+                auth: false,
+                policies: [],
+            },
+        },
+        {
+            method: 'PUT',
+            path: '/client/kyc',
+            handler: 'api::client.client.KYC',
+            config: {
+                auth: false,
+                policies: [],
+            },
+        },
+        {
+            method: 'PUT',
+            path: '/client/updateData',
+            handler: 'api::client.client.updateData',
+            config: {
+                auth: false,
+                policies: [],
+            },
+        },
+        {
+            method: 'GET',
+            path: '/client/public-address/:address',
+            handler: 'api::client.client.getByPublicAddress',
+            config: {
+                auth: false,
+                policies: [],
+            },
+        },
+        {
+            method: 'POST',
+            path: '/client/kyc/one-time-link',
+            handler: 'api::client.client.generateOneTimeLinkForKyc',
+            config: {
+                auth: false,
+                policies: [],
+            },
+        },
+        {
+            method: 'POST',
+            path: '/client/kyc/withPersonaStatus',
+            handler: 'api::client.client.withPersonaStatus',
+            config: {
+                auth: false,
+                policies: [],
+            },
+        },
+    ],
 };
