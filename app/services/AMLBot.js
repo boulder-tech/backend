a568924098e264a42afc10df46eec31b2bb70875
pablo-btech <pablo@bouldertech.fi>
2024-06-28 15:07:26 -0300

    create applicant

--- app/services/AMLBot.js
+++ app/services/AMLBot.js
@@ -1,19 +1,23 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const AML_BOT_API_URL = strapi.config.get('environments.personaApiURL');
+const AML_BOT_API_URL = strapi.config.get("environments.amlBotApiUrl");
 
 class AMLBot {
   constructor({ token }) {
     this.token = token;
   }
 
-  async createApplicant({ data }) {
+  async createApplicant(data) {
     const response = await axios.post(`${AML_BOT_API_URL}/applicants`, data, {
       headers: { Authorization: `Token ${this.token}` },
     });
 
     return response.data;
   }
+
+  // async getFormURL(data) {
+  //   const response = await axios.post()
+  // }
 }
 
 module.exports = AMLBot;
